52632e744a8b80d66273f546b5b8da8753f6a67d
sukhjinder2003 <sukhjindertw110@gmail.com>
2026-01-18 21:55:54 +0530

    Add Experiment-1 PDF and move project into project folder

--- experiment-1/project/src/App.jsx
+++ experiment-1/project/src/App.jsx
@@ -1,15 +1,14 @@
 import DashBoard from "./pages/DashBoard.jsx";
 import Log from "./pages/Log.jsx";
-// import useWindowState  from "./hooks/UseWindowState.jsx";
 
  
 
 function App() {
-  // const [width , height] = useWindowState;
+  
   return (<>
     <DashBoard />
     <p>Welcome to EcoTrack!</p>
-    {/* <p>{width}and {height}</p> */}
+    {}
     <Log />
   </>
     
